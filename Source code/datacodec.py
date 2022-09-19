ca32b3078d748d24cefd3887ad867931b01cf60b
Ivan Fernandez Alonso <95512993+IFAlonso@users.noreply.github.com>
2022-09-19 20:16:41 +0100

    Update datacodec.py

--- Source code/datacodec.py
+++ Source code/datacodec.py
@@ -1,4 +1,4 @@
-class Encoder():
+class Encoder_sensors():
 
   def __init__(self, mcu_id, temp, hum, light):
 
@@ -7,42 +7,63 @@ class Encoder():
     self.hum = hum
     self.light = light
 
-  def conversion(self):
-
-    global temp, hum, light, unit_id
-
-    unit_id = str(self.mcu_id)
-    temp = str(self.temp)
-    hum = str(self.hum)
-    light = str(self.light)
-
   def coding(self):
 
-    global datapacket
+    global unit_id, temp, hum, light, datapacket
 
-    if len(temp) == 1:
-      data_temp = 'T' + '0' + temp
-    elif len(temp) == 2:
-      data_temp = 'T' + temp
+    unit_id = 'ID' + str(self.mcu_id)
+    
+    if len(str(self.temp)) == 0:
+      data_temp = 'T' + '00' + str(sefl.temp)
+    elif len(str(self.temp)) == 1:
+      data_temp = 'T' + '0' + str(self.temp)
+    elif len(str(self.temp)) == 2:
+      data_temp = 'T' + str(self.temp)
 
-    if len(hum) == 1:
-      data_hum = 'H' + '00' + hum
-    elif len(hum) == 2:
-      data_hum = 'H' + '0' + hum
-    elif len(hum) == 3:
-      data_hum = 'H' + str(hum)
+    if len(str(self.hum)) == 0:
+      data_hum = 'H' + '000' + str(self.hum)
+    elif len(str(self.hum)) == 1:
+      data_hum = 'H' + '00' + str(self.hum)
+    elif len(str(self.hum)) == 2:
+      data_hum = 'H' + '0' + str(self.hum)
+    elif len(str(self.hum)) == 3:
+      data_hum = 'H' + str(self.hum)
 
-    if len(light) == 1:
-      data_light = 'L' + '000' + light
-    elif len(light) == 2:
-      data_light = 'L' + '00' + light
-    elif len(light) == 3:
-       data_light = 'L' + '0' + light
-    elif len(light) == 4:
-       data_light = 'L' + light
+    if len(str(self.light)) == 0:
+      data_light = 'L' + '0000' + str(self.light)
+    elif len(str(self.light)) == 1:
+      data_light = 'L' + '000' + str(self.light)
+    elif len(str(self.light)) == 2:
+      data_light = 'L' + '00' + str(self.light)
+    elif len(str(self.light)) == 3:
+       data_light = 'L' + '0' + str(self.light)
+    elif len(str(self.light)) == 4:
+       data_light = 'L' + str(self.light)
 
     datapacket = unit_id + data_temp + data_hum + data_light
 
+  def output(self):
+
+    return datapacket
+
+
+
+class Encoder_switches():
+
+  def __init__(self, mcu_id, switch1, switch2):
+
+    self.mcu_id = mcu_id
+    self.switch1 = switch1
+    self.switch2 = switch2
+    
+  def coding(self):
+
+    global unit_id, switch1, switch2, datapacket
+
+    unit_id = 'ID' + str(self.mcu_id)
+    switch1 = 'SW1' + str(self.switch1)
+    switch2 = 'SW2' + str(self.switch2)
+    datapacket = unit_id + switch1 + switch2
 
   def output(self):
 
@@ -71,3 +92,14 @@ class Decoder():
     light = data[9:13]
 
     return unit_id, temperature, humidity, light
+
+
+## EXAMPLE
+board = 1
+temperature = 23
+humidity = 55
+lux = 105
+
+sensor1 = Encoder_sensors(board, temperature, humidity, lux)
+sensor1.coding()
+print(sensor1.output())
